267f35a6880ef7be7861a31445027aceb8a10ecb
Hagar <hagermoh21@gmail.com>
2024-02-16 11:11:03 +0200

    Day 4

---
+++ Day 4/Counter II.js
@@ -0,0 +1,24 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let begin = init
+    return{
+        increment () {
+            return ++init;
+        },
+        decrement () {
+            return --init;
+        },
+        reset () {
+            return init=begin;
+        }
+    };
+};
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
